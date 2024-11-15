1bfbc84d682b94127d77fc7ddf868116e4f85b4b
Jenifer Andrea Gómez <jenifergomez017@gmail.com>
2024-11-15 15:36:39 -0500

    Asignación de anotaciones para Redis y servicios y controladores de cases ComponenteRepair y Device Detail

--- src/main/java/com/uptc/frw/casoestudioredis/controller/DeviceDetailController.java
+++ src/main/java/com/uptc/frw/casoestudioredis/controller/DeviceDetailController.java
@@ -5,6 +5,9 @@ import com.uptc.frw.casoestudioredis.jpa.model.DeviceDetail;
 import com.uptc.frw.casoestudioredis.services.ClientService;
 import com.uptc.frw.casoestudioredis.services.DeviceDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,11 +17,25 @@ import java.util.List;
 public class DeviceDetailController {
     @Autowired
     public DeviceDetailService deviceDetailService;
+
     @GetMapping
+    @Cacheable(
+            value = "DeviceDetail",
+            key = "'allDeviceDetail'",
+            unless = "#result == null"
+    )
+
     public List<DeviceDetail> getAllDeviceDetails(){
         return deviceDetailService.findAllDeviceDetails();
     }
+
     @GetMapping("{id}")
+    @Cacheable(
+            value = "DeviceDetail",
+            key = "#id",
+            unless = "#result == null"
+    )
+
     public DeviceDetail getDeviceDetail(@PathVariable long id){
         return deviceDetailService.findByIdDeviceDetail(id);
     }
@@ -26,12 +43,24 @@ public class DeviceDetailController {
     public DeviceDetail addDeviceDetail(@RequestBody DeviceDetail deviceDetail){
         return deviceDetailService.saveDeviceDetail(deviceDetail);
     }
+
     @PutMapping
+    @CachePut(
+            value = "DeviceDetail",
+            key = "#devicedetail.iddevicedetail",
+            unless = "#result == null"
+    )
+
     public DeviceDetail updateDeviceDetail(@RequestBody DeviceDetail deviceDetail){
         return deviceDetailService.updateDevClient(deviceDetail);
     }
 
     @DeleteMapping
+    @CacheEvict(
+            value = "DeviceDetail",
+            key = "#id"
+    )
+
     public void deleteDeviceService(@RequestParam long id){
         deviceDetailService.deleteDeviceDetail(id);
     }
--- src/main/java/com/uptc/frw/casoestudioredis/services/DeviceDetailService.java
+++ src/main/java/com/uptc/frw/casoestudioredis/services/DeviceDetailService.java
@@ -1,7 +1,6 @@
 package com.uptc.frw.casoestudioredis.services;
 
-import com.uptc.frw.casoestudioredis.jpa.model.Client;
-import com.uptc.frw.casoestudioredis.jpa.model.DeviceDetail;
+import com.uptc.frw.casoestudioredis.jpa.model.*;
 import com.uptc.frw.casoestudioredis.jpa.repository.ClientRepository;
 import com.uptc.frw.casoestudioredis.jpa.repository.DeviceDetailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,10 @@ import java.util.List;
 public class DeviceDetailService {
     @Autowired
     private DeviceDetailRepository deviceDetailRepository;
+    @Autowired
+    private ElectronicDeviceService electronicDeviceService;
+    @Autowired
+    private ComponentService componentService;
 
     public List<DeviceDetail> findAllDeviceDetails() {
         return deviceDetailRepository.findAll();
@@ -21,14 +24,21 @@ public class DeviceDetailService {
         return deviceDetailRepository.findById(id).orElse(null);
     }
     public DeviceDetail saveDeviceDetail(DeviceDetail deviceDetail){
+        ElectronicDevice electronicDevice = electronicDeviceService.findByIdElectronicDevice(deviceDetail.getIdElectronicDevice());
+        Component component = componentService.findByIdComponent(deviceDetail.getIdComponent());
+        deviceDetail.setElectronicDevice(electronicDevice);
+        deviceDetail.setComponent(component);
         return deviceDetailRepository.save(deviceDetail);
     }
+
     public DeviceDetail updateDevClient(DeviceDetail deviceDetail){
+        ElectronicDevice electronicDevice = electronicDeviceService.findByIdElectronicDevice(deviceDetail.getIdElectronicDevice());
+        Component component = componentService.findByIdComponent(deviceDetail.getIdComponent());
         DeviceDetail deviceDetail1= findByIdDeviceDetail(deviceDetail.getIdDeviceDetail());
         deviceDetail1.setQuantityDeviceDetail(deviceDetail.getQuantityDeviceDetail());
         deviceDetail1.setPriceDeviceDetail(deviceDetail.getPriceDeviceDetail());
-        deviceDetail1.setIdComponent(deviceDetail.getIdComponent());
-        deviceDetail1.setElectronicDevice(deviceDetail.getElectronicDevice());
+        deviceDetail1.setComponent(component);
+        deviceDetail1.setElectronicDevice(electronicDevice);
         return deviceDetailRepository.save(deviceDetail1);
     }
     public void deleteDeviceDetail(long id){
